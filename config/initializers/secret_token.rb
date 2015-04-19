3bf059c8183e7c3cf44fa58f92ade33f6aef7b31
reginald <regi103@gmail.com>
2015-04-19 14:31:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haiti2016::Application.config.secret_token = 'af245def816cc388ae365e64d171707ae9e6508b6bbdcc099b5a14033f2ee9287a303a0d04aa35787f0a32a926f0d0d80b65298e7de6f7b489dcd496a661f36a'
